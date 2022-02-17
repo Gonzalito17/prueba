d28b9c7d545aee31dce80604382c0ad0abb2ae5d
David <davidsitorico@gmail.com>
2022-02-17 15:53:40 -0500

    23 enero

--- construccion.py
+++ construccion.py
@@ -3,6 +3,11 @@ motor2= 1
 motor3= 30
 
 if motor3==90:
+    print("posicion correcta de 90°")
+    
+if motor1==("bajo"):
     print("posicion correcta")
     
-
+if motor2==1:
+    print("esta abierto")
+    
